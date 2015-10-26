4c9dac55d2419ee1d90caedf4d86c7ebda941fce
Mitali13 <mitaligajbhiye13@gmail.com>
2015-10-26 14:05:02 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,3 +1,5 @@
+
+
 #include <stdio.h>
 #include <stdlib.h>
 #include "mp.h"
@@ -6,6 +8,7 @@
 int ManagerMenu() {
 	int choice4;	
 	long int i, j;
+	
 	printf ("\nEnter 1 to add to good list.");
 	printf ("\nEnter 2 to edit the good list.");
 	printf ("\nEnter 3 to display list of goods.");
@@ -14,36 +17,55 @@ int ManagerMenu() {
 	printf ("\nEnter 6 for Billing.");
 	printf ("\nEnter 7 to EXIT\n");
 	scanf("%d", &choice4);
+
 	return choice4;
+
+	
 }
 
 
 
 int main() {
+
 	int choice4;
 	long int i, j;
 	printf ("\t\t\tWELCOME TO DEPARTMENTAL STORE MANAGEMENT SYSTEM\n\n");
 	printf ("\nLoading");
+	
 	while(1) {
 		choice4 = ManagerMenu();
+	
 		switch (choice4){
+	
 			case 1:GoodsEntry();
 				break;
+	
 			case 2:EditGoods();
 				break;
+	
 			case 3:GoodsListDisplay();
 				break;
+	
 			case 4:TotalSalesDisplay();
 				break;	
+	
 			case 5:DeleteGoods();
 				break;
+	
 			case 6:Billing();
 				break;
+	
 			case 7:exit(1);
 				break;
+	
 			default:printf("\nInvalid Selection!\nEnter again:\n");
+	
 		}
+	
 	}
+	
 	return 0;
+
 }
+
 
